13cae5c1136313d2b0249e223e3a12daad5711fd
322Diego <diegocordovamisaico@gmail.com>
2024-05-21 00:31:27 -0500

    MapeoDeEntidades

---
+++ FISCAapp/FISCA.Dominio/Entidades/Inscripciones_Asignaturas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FISCA.Dominio.Entidades
+{
+    public class Inscripciones_Asignaturas
+    {
+
+        [Key] public int IdInscripcion { get; set; }
+        public int IdAsignatura { get; set; }
+        public int IdEstudiante { get; set; }
+        public DateTime? FechaInscripcion { get; set; }
+        public string? Observaciones { get; set; }
+
+    }
+}
