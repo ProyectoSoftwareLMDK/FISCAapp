899182c975fe2182fe72fa05d02b1e6cd2debe4b
322Diego <diegocordovamisaico@gmail.com>
2024-05-21 16:43:19 -0500

    A

--- FISCAapp/FISCA.Aplicacion/obj/Debug/net8.0/FISCA.Aplicacion.AssemblyInfo.cs
+++ FISCAapp/FISCA.Aplicacion/obj/Debug/net8.0/FISCA.Aplicacion.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FISCA.Aplicacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b17919ed47dc9ebe64ccc8b3cde4d776185e6f68")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51818a41dfe49f7a38b1c7334fd3151bab7223b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("FISCA.Aplicacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FISCA.Aplicacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
