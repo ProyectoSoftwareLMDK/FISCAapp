b6d4dec14cf91f390058cc84af7063fd770221a7
LucasPaulAltamizaJulian <q01101g@upla.edu.pe>
2024-05-21 16:01:23 -0500

    Front
    a

--- FISCAapp/FISCAapp.Web/obj/Debug/net8.0/FISCAapp.Web.AssemblyInfo.cs
+++ FISCAapp/FISCAapp.Web/obj/Debug/net8.0/FISCAapp.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FISCAapp.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efabde59e109366176bb196ec5f37a9dfc49da87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26e039d7d106d9a30c108f892184c15da08b5766")]
 [assembly: System.Reflection.AssemblyProductAttribute("FISCAapp.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FISCAapp.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
