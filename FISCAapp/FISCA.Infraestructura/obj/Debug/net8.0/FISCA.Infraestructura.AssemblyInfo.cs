2c4991e53174ee0cbb9f87aef81b04e75ef6f316
LucasPaulAltamizaJulian <q01101g@upla.edu.pe>
2024-07-08 00:00:52 -0500

    Loginrutas
    validar login y no permitir aacceso por ruta

--- FISCAapp/FISCA.Infraestructura/obj/Debug/net8.0/FISCA.Infraestructura.AssemblyInfo.cs
+++ FISCAapp/FISCA.Infraestructura/obj/Debug/net8.0/FISCA.Infraestructura.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FISCA.Infraestructura")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbd2b8c42240b15f1921a783cd2345e7ae458594")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+854e869d0a4361f75df3b860f896b1ec7ce00d00")]
 [assembly: System.Reflection.AssemblyProductAttribute("FISCA.Infraestructura")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FISCA.Infraestructura")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- FISCAapp/FISCAapp.Web/obj/Debug/net8.0/FISCAapp.Web.AssemblyInfo.cs
+++ FISCAapp/FISCAapp.Web/obj/Debug/net8.0/FISCAapp.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FISCAapp.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5cdb838020c437b1ed28bf5678e34d52279d77f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+854e869d0a4361f75df3b860f896b1ec7ce00d00")]
 [assembly: System.Reflection.AssemblyProductAttribute("FISCAapp.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FISCAapp.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
