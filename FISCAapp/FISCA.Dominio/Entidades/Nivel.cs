fbb0d4f45e385b4621ac19c5e25fbafa49002058
Kenx-20 <kenyi13.2017.lg@gmail.com>
2024-07-08 17:29:06 -0500

    abc

--- FISCAapp/FISCA.Dominio/Entidades/Nivel.cs
+++ FISCAapp/FISCA.Dominio/Entidades/Nivel.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace FISCA.Dominio.Entidades
 {
+
     public class Nivel
     {
         [Key]
--- FISCAapp/FISCAapp.Web/obj/Debug/net8.0/FISCAapp.Web.AssemblyInfo.cs
+++ FISCAapp/FISCAapp.Web/obj/Debug/net8.0/FISCAapp.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FISCAapp.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03a74bdbdb38f8b3b549d31e603793543e4afc58")]
 [assembly: System.Reflection.AssemblyProductAttribute("FISCAapp.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FISCAapp.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
