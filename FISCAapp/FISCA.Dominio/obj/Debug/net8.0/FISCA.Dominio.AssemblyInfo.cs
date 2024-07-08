ba8e5e4f972076b046eaed39da0621f5b96661c0
MoisesOsccoHuarocc <113782529+MoisesOsccoHuarocc@users.noreply.github.com>
2024-07-08 00:29:10 -0500

    M

--- FISCAapp/FISCA.Dominio/obj/Debug/net8.0/FISCA.Dominio.AssemblyInfo.cs
+++ FISCAapp/FISCA.Dominio/obj/Debug/net8.0/FISCA.Dominio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FISCA.Dominio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+854e869d0a4361f75df3b860f896b1ec7ce00d00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83566412f6498c716f6814fa50abd5b86053d50e")]
 [assembly: System.Reflection.AssemblyProductAttribute("FISCA.Dominio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FISCA.Dominio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- FISCAapp/FISCAapp.Web/obj/Debug/net8.0/FISCAapp.Web.AssemblyInfo.cs
+++ FISCAapp/FISCAapp.Web/obj/Debug/net8.0/FISCAapp.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FISCAapp.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+854e869d0a4361f75df3b860f896b1ec7ce00d00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83566412f6498c716f6814fa50abd5b86053d50e")]
 [assembly: System.Reflection.AssemblyProductAttribute("FISCAapp.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FISCAapp.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
