034aa34c02f89d8fef766e60dbd171fba1216453
LucasPaulAltamizaJulian <q01101g@upla.edu.pe>
2024-07-08 22:50:13 -0500

    g
    f

--- FISCAapp/FISCA.Aplicacion/obj/Debug/net8.0/FISCA.Aplicacion.AssemblyInfo.cs
+++ FISCAapp/FISCA.Aplicacion/obj/Debug/net8.0/FISCA.Aplicacion.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FISCA.Aplicacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a42a4a463c8ec62906907e4e277d3abc41061237")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09d675412cb21a886f35e53b59ed49c31e05a057")]
 [assembly: System.Reflection.AssemblyProductAttribute("FISCA.Aplicacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FISCA.Aplicacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
