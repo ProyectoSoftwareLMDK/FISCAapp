26ade444c4426a4a09563d04cc631ffebcfc8d35
LucasPaulAltamizaJulian <q01101g@upla.edu.pe>
2024-07-09 23:22:40 -0500

    Tengo suenio

--- FISCAapp/FISCAapp.Web/obj/Debug/net8.0/FISCAapp.Web.AssemblyInfo.cs
+++ FISCAapp/FISCAapp.Web/obj/Debug/net8.0/FISCAapp.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FISCAapp.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a22c659c37b7a2a196854fcc11167c46978c1104")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82f448be1ea2f67ef42b9457b7d183c55be954b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("FISCAapp.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FISCAapp.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
