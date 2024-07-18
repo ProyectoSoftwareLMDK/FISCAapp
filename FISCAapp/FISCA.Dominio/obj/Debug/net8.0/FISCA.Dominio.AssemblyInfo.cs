83ea5c0a3fc54aded82212c865572a461326aaac
LucasPaulAltamizaJulian <q01101g@upla.edu.pe>
2024-07-18 18:12:21 -0500

    waza

--- FISCAapp/FISCA.Dominio/obj/Debug/net8.0/FISCA.Dominio.AssemblyInfo.cs
+++ FISCAapp/FISCA.Dominio/obj/Debug/net8.0/FISCA.Dominio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FISCA.Dominio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0582d7caedaa21f0fe153884a9c12eb5a5c0fd5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cc4a0b55be5413d46e13cd307a5c5c8491bfbf9")]
 [assembly: System.Reflection.AssemblyProductAttribute("FISCA.Dominio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FISCA.Dominio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
